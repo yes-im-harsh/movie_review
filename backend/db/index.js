bbf808131ecad2fba64f822a5f0163a18500dbbd
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-18 02:18:14 +0530

    Mongoose Model Schema

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,5 +4,3 @@ mongoose
   .connect("mongodb://localhost:27017/review_app")
   .then(() => console.log("db is connected"))
   .catch((ex) => console.log("db connection failed", ex));
-
-  //* Throwing Error, put the write connection URL, Install Mongodb locally.
