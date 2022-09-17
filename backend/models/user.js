a31e9522d04e890615c17ae7f5ce6a5a70a750e8
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-18 02:39:29 +0530

    Hashing The Password using bcrypt

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -16,6 +17,15 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const encyptedPassword = await bcrypt.hash(this.password, 10);
+    this.password = encyptedPassword;
+  }
+
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
