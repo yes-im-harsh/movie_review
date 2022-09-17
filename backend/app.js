47273f46d71b6ee09f1932bacc800d42bd242bb1
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-17 22:17:41 +0530

    First Express Server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express = require("express");
+
+const app = express();
+
+// console.log(app)
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello I am from the backend :) </h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Hello I am from the backend /about :) </h1>");
+});
+
+app.listen(8000, () => {
+  console.log("The port is listing on 8000");
+});
