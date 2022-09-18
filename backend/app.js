1c4465f981fb89271c402aa6ab5ca2e96ea158a8
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-18 12:46:33 +0530

    Preventing Duplicate User & Middlewares inside Express

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,25 @@ app.use(express.json());
 
 app.use("/api/user", userRouter);
 
-app.get("/about", (req, res) => {
-  res.send("<h1>Hello I am from the backend /about :) </h1>");
-});
+// app.get("/about", (req, res) => {
+//   res.send("<h1>Hello I am from the backend /about :) </h1>");
+// });
+
+//Middleware
+// app.post("/", middleware () => {}, (req,res)=> {})
+
+app.post(
+  "/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.json({ error: "email/password missing :(" });
+    next();
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am Sign-in</h1>");
+  }
+);
 
 app.listen(8000, () => {
   console.log("The port is listing on 8000");
