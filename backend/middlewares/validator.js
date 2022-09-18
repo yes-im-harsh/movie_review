b24566ad49236a8d4ce85700cf01f550f0240c58
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-18 13:40:04 +0530

    Implemented Express Validator Middleware. Validating name, Email & Password

---
+++ backend/middlewares/validator.js
@@ -0,0 +1,23 @@
+const { check, validationResult } = require("express-validator");
+
+exports.userValidator = [
+  check("name").trim().not().isEmpty().withMessage("Name is Missing!"),
+  check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+  check("password")
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage("Password is missing!")
+    .isLength({ min: 8, max: 20 })
+    .withMessage("Password must be 8 to 20 characters long!"),
+];
+
+exports.validate = (req, res, next) => {
+  const error = validationResult(req).array();
+  // console.log(error);
+  if (error.length) {
+    return res.json({ error: error[0].msg });
+  }
+
+  next();
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,21 +1,9 @@
 const express = require("express");
 const { create } = require("../controllers/user");
+const { userValidator, validate } = require("../middlewares/validator");
 
 const router = express.Router();
 
-//post request*, will be getting error of not Cannot GET /create-user
-router.post("/create", create);
+router.post("/create", userValidator, validate, create);
 
 module.exports = router;
-
-//(1)
-
-// const express = require("express");
-
-// const router = express.Router();
-
-// router.get("/", (req, res) => {
-//   res.send("<h1>This is coming from backend routes :)</h1>");
-// });
-
-// module.exports = router;
