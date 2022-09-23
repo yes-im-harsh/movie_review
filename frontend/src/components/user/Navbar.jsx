f18bf3a978ab0e667f03e8117b0840d1fd20d2ef
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-23 23:58:48 +0530

    Using React-icons

--- frontend/src/components/user/Navbar.jsx
+++ frontend/src/components/user/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillSunFill } from "react-icons/bs";
 
 const Navbar = () => {
   return (
@@ -6,8 +7,20 @@ const Navbar = () => {
       <div className="text-white max-w-screen-xl mx-auto p-2">
         <div className="flex justify-between items-center">
           <img src="./logo.png" alt="" className="h-10" />
-          <ul>
-            <li>Login</li>
+
+          <ul className="flex items-center space-x-4">
+            <li>
+              <button className="bg-dark-subtle p-1 rounded">
+                <BsFillSunFill className=" text-white" size={24} />
+              </button>
+            </li>
+            <li>
+              <input
+                type="text"
+                className="border-2 border-dark-subtle p1 rounded bg-transparent text-xl outline-none focus:border-white"
+              />
+            </li>
+            <li className="text-white font-semibold text">Login</li>
           </ul>
         </div>
       </div>
