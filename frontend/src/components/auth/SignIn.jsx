fe86b482619d41b11495d0d63e547426cc1352c1
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-24 23:52:59 +0530

    Refactoring SignIn Form

--- frontend/src/components/auth/SignIn.jsx
+++ frontend/src/components/auth/SignIn.jsx
@@ -1,29 +1,20 @@
 import React from "react";
 import Container from "../Container";
+import FormInput from "../form/FormInput";
+import Title from "../form/Title";
 
 const SignIn = () => {
   return (
     <div className="fixed inset-0 bg-primary -z-20 flex justify-center items-center">
       <Container>
-        <form className="bg-secondary rounded p-6 w-72">
-          <h1 className="text-xl font-semibold text-white text-center">
-            Sign In
-          </h1>
-          <div className="flex flex-col-reverse">
-            {/* <label className="font-semibold text-dark-subtle" htmlFor="email">Email</label> */}
-            <input
-              type="text"
-              id="email"
-              className="bg-transparent rounded border-2 border-dark-subtle focus:border-white w-full text-lg outline-none p-1 text-white peer transition"
-              placeholder="jhon@gmail.com"
-            />
-            <label
-              className="font-semibold text-dark-subtle peer-focus:text-white transition self-start"
-              htmlFor="email"
-            >
-              Email
-            </label>
-          </div>
+        <form className="bg-secondary rounded p-6 space-y-6 w-72">
+          <Title>Sign In</Title>
+          <FormInput label="Email" placeholder="jhon@gmail.com" name="email" />
+          <FormInput
+            label="Password"
+            placeholder="**********"
+            name="password"
+          />
         </form>
       </Container>
     </div>
---
+++ frontend/src/components/form/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({children}) => {
+  return (
+   <h1 className="text-xl font-semibold text-white text-center">
+   {children}
+ </h1>
+  )
+}
+
+export default Title
