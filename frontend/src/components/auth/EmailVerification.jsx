4b064afb6cbbe4f1a59e0483f026514b4689dd32
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-09-26 12:29:10 +0530

    Email verification OTP form UI

--- frontend/src/components/auth/EmailVerification.jsx
+++ frontend/src/components/auth/EmailVerification.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from "react";
+import Container from "../Container";
+import Submit from "../form/Submit";
+import Title from "../form/Title";
+
+const OPT_Length = 6;
 
 const EmailVerification = () => {
+  const [otp, setOtp] = useState(new Array(OPT_Length).fill(""));
   return (
-    <div>EmailVerification</div>
-  )
-}
+    <div className="fixed inset-0 bg-primary -z-20 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 space-y-6">
+          <div>
+            <Title>Please enter the OTP to verify your account</Title>
+            <p className="text-center text-dark-subtle">
+              OTP has been sent to your Email
+            </p>
+          </div>
 
-export default EmailVerification
+          <div className="flex justify-center items-center space-x-4">
+            {otp.map((_, index) => {
+              return (
+                <input
+                  type="number"
+                  className="w-12 h-12 border-2 border-dark-subtle focus:border-white rounded bg-transparent outline-none text-center text-white font-semibold text-xl spin-button-none"
+                />
+              );
+            })}
+          </div>
+
+          <Submit value="Send Link" />
+        </form>
+      </Container>
+    </div>
+  );
+};
+
+export default EmailVerification;
