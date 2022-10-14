26bbd6f418e30cff9e34732eb5a4f7c7b4cdff6f
yes-im-harsh <harshchauhan.cse279@gmail.com>
2022-10-14 23:14:02 +0530

    Rendering Reset Password isVerifying Indicator

--- frontend/src/components/auth/ConfirmPassword.jsx
+++ frontend/src/components/auth/ConfirmPassword.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useState } from "react";
+import { ImSpinner10 } from "react-icons/im";
 import { useSearchParams } from "react-router-dom";
 import { commonModalClasses } from "../../utils/theme";
 import Container from "../Container";
@@ -8,13 +10,29 @@ import Submit from "../form/Submit";
 import Title from "../form/Title";
 
 const ConfirmPassword = () => {
+  const [isVerifying, setIsVerifying] = useState(true);
   const [searchParams] = useSearchParams();
   const token = searchParams.get("token");
   const id = searchParams.get("id");
   console.log(token, id);
   // http://localhost:3000/auth/reset-password?token=6bab0f8391dbb7cf34dbfb58c347a3b2d7683c536cd5d640d39f67f3e948&id=6349959b1f099e97fafbd261
 
-  //To-do: isValid, isVerifying, !isValid
+  //To-do: isValid, !isValid
+
+  if (isVerifying) {
+    return (
+      <FormContainer>
+        <Container>
+          <div className="flex space-x-2 items-center">
+            <h1 className="text-4xl font-semibold dark:text-white text-primary">
+              Wait! Token Verifying
+            </h1>
+            <ImSpinner10 className="animate-spin text-4xl dark:text-white text-primary" />
+          </div>
+        </Container>
+      </FormContainer>
+    );
+  }
 
   return (
     <FormContainer>
